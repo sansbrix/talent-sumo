66d6bf8b43a3231908b8c3f08583e817a35b66c9
iamkarantalwar <iamkarantalwar@gmail.com>
2022-08-10 00:29:32 +0530

    Added More Live Fixes

--- talentsumo/urls.py
+++ talentsumo/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings # to import static in deployment
+from django.conf.urls.static import static # to import static in deployment
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("backend.urls")),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # to import
